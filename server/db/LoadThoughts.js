ff0536c9133b0942c7e8078abf1da48d02786d95
puakehaulani <puakehaulani@gmail.com>
2021-03-03 20:21:10 -0800

    removed endpoints for local instance

--- server/db/LoadThoughts.js
+++ server/db/LoadThoughts.js
@@ -3,9 +3,10 @@ const fs = require('fs');
 
 AWS.config.update({
   region: "us-east-2",
-  endpoint: "http://localhost:8000"
+  // removed endpoint property when deploying to cloud 
+  // endpoint: "http://localhost:8000"
 });
-const dynamodb = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
+const dynamodb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
 
 console.log("Importing thoughts into DynamoDB. Please wait.");
 const allUsers = JSON.parse(fs.readFileSync('./server/seed/users.json', 'utf8'));
--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const AWS = require("aws-sdk");
 const awsConfig = {
   region: "us-east-2",
-  endpoint: "http://localhost:8000",
+  // removed endpoint property when deploying to cloud 
+  // endpoint: "http://localhost:8000",
 
 };
 AWS.config.update(awsConfig);
