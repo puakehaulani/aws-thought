190f7b87319008a31e699bab4aba7acd338846c7
puakehaulani <puakehaulani@gmail.com>
2021-03-04 16:35:37 -0800

    backend routes for images saving to s3, verified in insomnia

---
+++ server/utils/params-config.js
@@ -0,0 +1,14 @@
+const { v4: uuidv4 } = require('uuid');
+const params = fileName => {
+    const myFile = fileName.originalname.split('.');
+    const fileType = myFile[myFile.length - 1];
+
+    const imageParams = {
+        Bucket: 'user-images-d9c9b7b0-194e-459b-8473-78b4a84e9e29',
+        Key: `${uuidv4()}.${fileType}`,
+        Body: fileName.buffer
+    };
+
+    return imageParams;
+};
+module.exports = params;
